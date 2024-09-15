f8effa4752ec39db403a4471fb5c6592790f804a
Gabriela Assef <gabriela@assefcursos.com.br>
2024-09-15 19:47:13 -0300

    primeiro commit

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    {
+        titulo: "Gabriela Assef",
+        descricao: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        link: "https://www.instagram.com/gaassef",
+        tags: "palavras chave"
+    }
+    ,
+    {
+        titulo: "José Guilherme",
+        descricao: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        link: "https://www.instagram.com/gaassef",
+        tags: "palavras chave"
+    }
+    ,
+    {
+        titulo: "Wandinha",
+        descricao: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        link: "https://www.instagram.com/gaassef",
+        tags: "palavras chave"
+    }
+    
+];
+
